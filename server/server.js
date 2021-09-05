fbac429cedf3923eabc827bc23a724ee1265a2be
CrawleyJ2 <79888659+CrawleyJ2@users.noreply.github.com>
2021-09-05 18:39:45 -0400

    updates to server-side function and added schemas folder

--- server/server.js
+++ server/server.js
@@ -1,10 +1,16 @@
 const express = require('express');
 const path = require('path');
+const { ApolloServer } = require('apollo-server-express');
+const { authMiddleware } = require('./utils/auth');
+
 const db = require('./config/connection');
 const routes = require('./routes');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+// const server declaration
+
+// async gunction with server.start(); and applyMiddleware
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
